4b36b24c239e841d63e2e8101d3cdb306055e009
Shahbaz <shahbaz.ramzan@codedistrict.com>
2023-09-15 18:21:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
+};
+
+export default App;
